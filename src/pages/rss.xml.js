51c07dab7e52b0aa606b0d95de02f6a17f7a356e
Moosa Ibrahim <emailmusa@icloud.com>
2023-06-09 22:37:06 +0500

    rss feed  link updated

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: 'https://astro-blog-musa.netlify.app/clear',
+    site: 'https://astro-blog-musa.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
